41e31cc461667ece710621561ea1ef1a217b8d7a
Linux-Ginger <268384589+Linux-Ginger@users.noreply.github.com>
2026-03-21 21:28:35 +0100

    Fix UI

--- viewer/main_window.py
+++ viewer/main_window.py
@@ -21,6 +21,8 @@ import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw, GLib, GdkPixbuf, Gio, Gdk
+gi.require_foreign("cairo")
+import cairo
 
 try:
     import cairo
